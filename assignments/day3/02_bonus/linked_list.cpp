3ecf90ee9e7372361d9f6bff6e846bf4da878f2f
Rickard Ljung <rickardljung1@gmail.com>
2020-10-22 07:51:29 +0200

    fixed bug when searching for element. Adding two customers in main. Remove function not implemented yet

--- assignments/day3/02_bonus/linked_list.cpp
+++ assignments/day3/02_bonus/linked_list.cpp
@@ -3,7 +3,7 @@
 
 list_element * LinkedList::findElement(unsigned int index){
     list_element *current_element = this->first_element;
-    for(int i = 0; i < index-1; i++) {
+    for(int i = 0; i < index; i++) {
         current_element = current_element->next;
     }
     return current_element;
@@ -31,61 +31,9 @@ void LinkedList::add(void *object, unsigned int index) {
 }
 
 void LinkedList::remove(unsigned int index) {
-
+//TODO
 }
 
 void * LinkedList::getObject(unsigned int index) {
     return findElement(index)->object;
 }
-
-/*
-int main()
-{
-    bool exit = false;
-    list_element* first_element = nullptr;
-    while(!exit) {
-        std::cout << "\nChoose an alternative from the menu below: (1-4) \n1. Add new item to the end of the list\n2. Remove item from the front of the list\n3. Print the list\n4. Exit" << std::endl;
-        int user_input;
-        std::cin >> user_input;
-        if(user_input == 1) { //New item
-            int user_number;
-            std::cout << std::endl << "Specify a number to add to the list: " << std::endl;
-            std::cin >> user_number;
-            list_element *current_element = first_element;
-            if (first_element == nullptr) {
-                first_element = new list_element;
-                current_element = first_element;
-            } else {
-                int user_index;
-                std::cout << std::endl << "Specify the index to add the new number to (zero indexed): " << std::endl;
-                std::cin >> user_index;
-                if (user_index == 0) { //add first in list
-                    current_element = new list_element;
-                    current_element->next = first_element;
-                    first_element = current_element;
-                } else {
-                    for(int i = 0; i < user_index-1; i++) { //search element to add the new element after
-                        current_element = current_element->next;
-                    }
-                    list_element *next_element = current_element->next;
-                    current_element->next = new list_element;
-                    current_element = current_element->next;
-                    current_element->next = next_element;
-                }
-            }
-            current_element->number = user_number;
-
-        } else if(user_input == 2) { //Remove item
-            list_element *remove_element = first_element;
-            first_element = first_element->next;
-            delete remove_element;
-        } else if(user_input == 3) { //Print
-            print(first_element);
-        } else if(user_input == 4) { //Exit
-            exit = true;
-        } else {
-            std::cout << std::endl << "Invalid input, please try again!" << std::endl;
-        }
-    }
-}
-*/
--- assignments/day3/02_bonus/main.cpp
+++ assignments/day3/02_bonus/main.cpp
@@ -5,12 +5,21 @@ int main () {
     Record *record = new Record("Rickard Ljung", "20201021", service_enum::TireChange);
     record->changePart("Tire", 40);
     record->changePart("SW upgrade", 626.66);
-
     record->pay(payment_method_enum::Swish, 13.37);
 
     LinkedList list;
     list.add(record, 0);
+    list.add(new Record("Goran", "20191224", service_enum::WindScreenChange), 0);
 
-    Record *record2 = static_cast<Record*>(list.getObject(0));
-    //record2->printRecordData();
+    record = static_cast<Record*>(list.getObject(0));
+    record->printRecordData();
+    record = static_cast<Record*>(list.getObject(1));
+    record->printRecordData();
+
+    record = static_cast<Record*>(list.getObject(0));
+    //list.remove(0);
+    delete record;
+    record = static_cast<Record*>(list.getObject(1)); //list.getObject(0)
+    //list.remove(0);
+    delete record;
 }
