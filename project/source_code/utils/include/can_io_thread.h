66e260fced8ff8dbdd0d602d3b9dd255d901c899
Rickard Ljung <rickardljung1@gmail.com>
2020-11-11 11:34:31 +0100

    changed name of can_reader and do both read and write to can in the same thread since socket is not thread safe. added transmit buffer to can_buffer

--- project/source_code/utils/include/can_io_thread.h
+++ project/source_code/utils/include/can_io_thread.h
@@ -7,17 +7,17 @@
 #include "socketcan.h"
 #include <string>
 
-class CanReader {
+class CanIOThread {
     private:
         std::thread read_can_thread;
         scpp::SocketCan *socket;
-        void Run(uint8_t message_id);
+        void Run(uint8_t message_id, uint8_t transmit_message_id);
         bool stop_thread = false;
         bool thread_stopped = false;
         bool socket_initialized = false;
     public:
-        CanReader(scpp::SocketCan *socket, uint8_t message_id);
-        ~CanReader();
+        CanIOThread(scpp::SocketCan *socket, uint8_t receive_message_id, uint8_t transmit_message_id);
+        ~CanIOThread();
         scpp::SocketCan * get_socket();
 };
 
--- project/source_code/utils/src/can_buffer.cpp
+++ project/source_code/utils/src/can_buffer.cpp
@@ -1,24 +1,52 @@
-//create a singleton class with RingBuffer
 //https://stackoverflow.com/questions/1008019/c-singleton-design-pattern
 #include "user_input.h"
 #include "can_buffer.h"
 #include <iostream>
 #include <cstring>
 
+/*!
+* Adds received can data to the receive (RX) buffer
+* @param data received can data
+*/
+void CanBuffer::AddRx(uint8_t data[]) {  //this will be replaced by ringbuffer
 
-void CanBuffer::Add(uint8_t data[]) {  //this will be replaced by ringbuffer
-    
     if(data!=nullptr)
     {
-        std::memcpy(this->received_can_data, data, sizeof(&data));  
+        std::memcpy(this->receive_can_data, data, sizeof(&data));
     }
     else
     {
         exit(-1);
-    } 
+    }
+}
+/*!
+* Adds data to be sent to the transmission (TX) buffer
+* @param data data to be transmitted
+*/
+void CanBuffer::AddTx(uint8_t data[]) {  //this will be replaced by ringbuffer
+
+    if(data!=nullptr)
+    {
+        std::memcpy(this->transmit_can_data, data, sizeof(&data));
+    }
+    else
+    {
+        exit(-1);
+    }
 }
 
+/*!
+* Pulls next data package from the receive (RX) buffer
+* @return received data
+*/
+uint8_t * CanBuffer::PullRx() {            //this will be replaced by ringbuffer
+    return this->receive_can_data;
+}
 
-uint8_t * CanBuffer::Pull() {            //this will be replaced by ringbuffer
-    return this->received_can_data;
+/*!
+* Pulls next data package from the transmit (TX) buffer
+* @return data to be transmitted
+*/
+uint8_t * CanBuffer::PullTx() {            //this will be replaced by ringbuffer
+    return this->transmit_can_data;
 }
