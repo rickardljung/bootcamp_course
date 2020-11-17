8200f4e45b32ccf5d9703784d5a3bc5c96d62995
lmodig93 <73157510+lmodig93@users.noreply.github.com>
2020-11-17 16:15:23 +0100

    Update engine_simulator.h

--- project/source_code/drivetrain/include/engine_simulator.h
+++ project/source_code/drivetrain/include/engine_simulator.h
@@ -16,7 +16,7 @@ class Engine {
     public:
         Engine(uint16_t hp, uint16_t max_rpm);
         void RPM(uint8_t acc_pedal, uint8_t brk_pedal, uint16_t sampletime);
-        void ActualRPM(uint8_t speed, double calc_const);
+        void ActualRPM(uint8_t speed, double speed_to_rpm_factor);
         void Ignition(bool ign_req, uint8_t speed, uint8_t brk_pedal, uint8_t gear_position);
         uint16_t get_eng_rpm();
         uint8_t get_eng_sts();
