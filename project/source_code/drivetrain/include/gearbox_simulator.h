005d692ee0c08fe185dfbf8b27a8f39c8d192229
lmodig93 <ludvigmodig@gmail.com>
2020-11-17 11:41:23 +0100

    Compilable skeleton of drivetrain

--- project/source_code/drivetrain/include/gearbox_simulator.h
+++ project/source_code/drivetrain/include/gearbox_simulator.h
@@ -7,8 +7,9 @@ class Gearbox {
         std::pair<uint8_t, double> *gear_ratio;
         uint8_t gear_lever_position;
     public:
+        Gearbox() = default; //remove when real constructor is implemented
         Gearbox(double *gear_ratio, uint8_t nbr_gears); //this->gear_ratio = new std::pair<uint8_t, double>(nbr_gears+1);
-        ~Gearbox(); //delete pair
+        ~Gearbox() = default;//TODO: delete pair
         void GearLeverPosition(uint8_t gear_pos_req, uint8_t speed, uint8_t brk_pedal);
         void GearNumber(uint16_t eng_rpm);
         double get_gear_ratio();
