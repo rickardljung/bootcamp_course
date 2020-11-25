bf66f3a95da50e806e0a0fb745b09039718c5efc
MarcinZwierzynski <marcin.zwierzynski@volvocars.com>
2020-11-25 15:04:27 +0100

    changes to can buffer

--- project/source_code/utils/include/can_buffer.h
+++ project/source_code/utils/include/can_buffer.h
@@ -5,9 +5,6 @@
 #include "can_buffer.h"
 #include "user_input.h"
 
-#include <thread>
-#include <cstring>
-#include <iostream>
 #include <unordered_map>
 
 
@@ -19,108 +16,17 @@ typedef struct CanData_struct{
     uint8_t length = 0;
 } CanData;
 
-template <int frames_to_send>
+
 class CanBuffer
 {
     public:
-        //constexpr uint8_t x = frames_to_send;
-
-        // static CanBuffer& GetInstance(){
-        //     static CanBuffer    instance;
-        //     return instance;
-        // }
-        // CanBuffer(CanBuffer const&)       = delete;
-        // void operator=(CanBuffer const&)  = delete;
         CanBuffer()=default;
-        // void AddTx(const uint32_t *id, uint8_t payload[], const uint8_t *length);
-        // void AddRx(const uint32_t *id, uint8_t payload[], const uint8_t *length);
-        // CanData PullTx();
-        // CanData PullRx();
-
-        void AddTx(const uint32_t *id, uint8_t payload[], const uint8_t *length);
-        CanData PullTx();
-
-        // bool ReceiveBufferEmpty();
-        // bool TransmitBufferEmpty();
-    private:
-        //UserInput received_CanData; //this will be replaced by ringbuffer
-        // CanData receive_candata[frames_to_send];
-        // CanData transmit_candata[frames_to_send];
-       
+        void CanBuffer::Add(const uint32_t& id, uint8_t payload[],const uint8_t& length);
+        CanData CanBuffer::Pull(uint32_t& frames_to_send);
+    private:    
         std::unordered_map<int, CanData> candata;
         std::mutex transmit_buffer_mutex;
-        
-        // bool receive_empty = true;
-        // bool transmit_empty = true;
-        // std::mutex receive_buffer_mutex;
-        // std::mutex transmit_buffer_mutex;
 };
-// /*!
-// * Check if receive buffer contains any elements
-// * @return TRUE if receive buffer is empty
-// */
-// template <int frames_to_send>
-// inline bool CanBuffer<frames_to_send>::ReceiveBufferEmpty()
-// {
-//     return this->receive_empty;
-// }
-// /*!
-// * Check if transmit buffer contains any elements
-// * @return TRUE if transmit buffer is empty
-// */
-// template <int frames_to_send>
-// inline bool CanBuffer<frames_to_send>::TransmitBufferEmpty()
-// {
-//     return this->transmit_empty;
-// }
 
-// /*!* Adds received can data to the receive (RX) buffer
-// * @param data received can data
-// */
-// template <int frames_to_send>
-// void CanBuffer<frames_to_send>::AddRx(const uint32_t *id, uint8_t payload[], const uint8_t *length)
-// {
-//     for()
-//     std::lock_guard<std::mutex> lock(receive_buffer_mutex);
-//     std::memcpy(this->receive_candata[frames_to_send].payload, payload, *length);
-//     this->receive_candata[frames_to_send].id = *id;
-//     this->receive_candata[frames_to_send].length = *length;
-//     this->receive_empty = false;
-// }
-/*!
-* Adds data to be sent to the transmit (TX) buffer
-* @param data data to be transmitted
-*/
-template <int frames_to_send>
-void CanBuffer<frames_to_send>::AddTx(const uint32_t *id, uint8_t payload[],const uint8_t *length)
-{
-    std::lock_guard<std::mutex> lock(transmit_buffer_mutex);
-    std::memcpy(this->transmit_candata[frames_to_send].payload, payload, *length);
-    this->transmit_candata[frames_to_send].id = *id;
-    this->transmit_candatap[frames_to_send].length = *length;
-    this->transmit_empty = false;
-}
-/*!
-* Pulls next data package from the receive (RX) buffer
-* @return received data
-*/
-template <int frames_to_send>
-CanData CanBuffer<frames_to_send>::PullRx()
-{
-    std::lock_guard<std::mutex> lock(receive_buffer_mutex);
-    this->receive_empty = true;
-    return this->receive_candata;
-}
-/*!
-* Pulls next data package from the transmit (TX) buffer
-* @return data to be transmitted
-*/
-template <int frames_to_send>
-CanData CanBuffer<frames_to_send>::PullTx()
-{
-    std::lock_guard<std::mutex> lock(transmit_buffer_mutex);
-    this->transmit_empty = true;
-    return this->transmit_candata;
-}
 
 #endif
--- project/source_code/utils/src/can_buffer.cpp
+++ project/source_code/utils/src/can_buffer.cpp
@@ -6,3 +6,27 @@
 #include "can_buffer.h"
 
 //Moved to .h 
+
+
+/*!
+* Adds data to be sent to the transmit (TX) buffer
+* @param data data to be transmitted
+*/
+
+void CanBuffer::Add(const uint32_t& id, uint8_t payload[],const uint8_t& length)
+{
+    std::lock_guard<std::mutex> lock(transmit_buffer_mutex);
+    std::memcpy(this->candata.payload, payload, length);
+    this->candata.id = id;
+    this->candatap.length = length;
+}
+
+/*!
+* Pulls next data package from the transmit (TX) buffer
+* @return data to be transmitted
+*/
+
+CanData CanBuffer::Pull(uint32_t& frames_to_send)
+{
+    std::lock_guard<std::mutex> lock(transmit_buffer_mutex);
+}
